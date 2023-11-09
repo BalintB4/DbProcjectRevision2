3d712400569ca52a20b996d9338f0b360ccb8cb6
HikariTempest <arwed@gmx.at>
2023-11-09 10:46:53 +0100

    docs: added infos
    feat: added the GeneTypeLookup Table

---
+++ sql/GeneTypeLookup.sql
@@ -0,0 +1,6 @@
+INSERT INTO GeneTypeLookup (gene_type_id, gene_type)
+VALUES ('0', 'Human');
+INSERT INTO GeneTypeLookup (gene_type_id, gene_type)
+VALUES ('1', 'Mouse');
+INSERT INTO GeneTypeLookup (gene_type_id, gene_type)
+VALUES ('2', 'Worm');
